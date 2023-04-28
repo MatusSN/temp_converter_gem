134edbaac162b7a45506f04d128322ce408d4c50
MatusSN <mr.qwerty1y@gmail.com>
2023-04-28 17:33:28 -0300

    add code

--- lib/temp_converter_gem.rb
+++ lib/temp_converter_gem.rb
@@ -3,6 +3,40 @@
 require_relative "temp_converter_gem/version"
 
 module TempConverterGem
-  class Error < StandardError; end
-  # Your code goes here...
+  class Convert 
+    class << self 
+      def to_celsius(scale, temp)
+        case scale 
+        when :fahrenheit, :f 
+          return (temp - 32) / 1.8 
+        when :kelvin, :k
+          return (temp - 273.15)
+        else 
+          raise ArgumentError, "Invalid scale"
+        end 
+      end 
+       
+      def to_fahrenheit(scale, temp)
+        case scale 
+        when :celsius, :c 
+          return (temp * 1.8) + 32 
+        when :kelvin, :k
+          return ((temp - 273.15) * 1.8) + 32
+        else 
+          raise ArgumentError, "Invalid scale"
+        end
+      end 
+
+      def to_kelvin(scale, temp)
+        case scale 
+        when :fahrenheit, :f 
+          return temp + 273.15
+        when :celsius, :c 
+          return ((temp - 32) / 1.8) + 273.15
+        else 
+          raise ArgumentError, "Invalid scale"
+        end 
+      end 
+    end 
+  end
 end
