c045fba2b2ab7467853f04fc3a943569aa2ed6ae
MatusSN <mr.qwerty1y@gmail.com>
2023-04-28 17:29:41 -0300

    first commit

---
+++ lib/temp_converter_gem.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "temp_converter_gem/version"
+
+module TempConverterGem
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/temp_converter_gem/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module TempConverterGem
+  VERSION = "0.1.0"
+end
